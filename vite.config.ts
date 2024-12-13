0265e7d6bb3cf9245dba9d5d8798cabe8b1d232f
Carl400 <cmwellman@gmail.com>
2024-12-13 14:12:14 +0100

    eslint, prettier, sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  'base': '/SNABB/' // TODO: Ändra till ert reponamn här
+  base: "/SNABB/", // TODO: Ändra till ert reponamn här
 });
